d607c612a9e9e5252f8298dd368a3aba8f10ef2b
Gursewak singh <gursewaksinghcse@gmail.com>
2025-11-13 09:21:36 +0530

    added new folders and if else first code

---
+++ Coder Army/Basics_Fundamentals/If Else Loop/lec-01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter your age : ";
+    cin>>n;
+
+    if(n>18){
+        cout<<"ADULT";
+
+    }
+    else
+    cout<<"minor";
+
+    return 0;
+}
