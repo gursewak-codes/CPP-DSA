c97119abd9740c038c5332ff5c9d1383bc38fce2
Gursewak singh <gursewaksinghcse@gmail.com>
2025-11-14 08:39:56 +0530

    added new folders for hw and lec ques

---
+++ Coder Army/BASICS/IF_ELSE LOOP/H.W questions/lec 04 .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b =4,10;
+    if(a>b)
+    {
+        cout<<"a is greater";
+    } 
+
+    else
+    {
+        cout<<"b is greater";
+    }
+
+
+}
